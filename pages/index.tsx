d52fc9bb573e106b9564e0ee4b652e5584ba1e3e
yaralahruthik <yaralahruthikreddy@gmail.com>
2022-01-24 14:57:09 +0530

    Final Output

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,48 @@
 import type { NextPage } from 'next';
 
+import classes from './index.module.css';
+import Me from '../public/me.webp';
+import Image from 'next/image';
+
 const Home: NextPage = () => {
-	return <>Home Page</>;
+  return (
+    <article className={classes.container}>
+      <div className={classes.line} />
+      <div className={classes.img}>
+        <Image src={Me} alt='Author' />
+      </div>
+      <div className={classes.intro}>
+        <h1 className={classes.title}>#iamYHR</h1>
+        <h2 className={classes.subTitleOne}>Developer? Writer? Photographer?</h2>
+        <h3 className={classes.subTitleTwo}>I do a lot of things!</h3>
+      </div>
+      <div className={classes.externalLinks}>
+        <span>
+          <a
+            target='_blank'
+            rel='noreferrer'
+            href='https://github.com/yaralahruthik'
+          >
+            Developer Portfolio
+          </a>
+        </span>
+        <span>
+          <a target='_blank' rel='noreferrer' href='https://iamyhr.medium.com/'>
+            Writing Portfolio
+          </a>
+        </span>
+        <span>
+          <a
+            target='_blank'
+            rel='noreferrer'
+            href='https://www.instagram.com/i_m_yhr/'
+          >
+            Photography Portfolio
+          </a>
+        </span>
+      </div>
+    </article>
+  );
 };
 
 export default Home;
